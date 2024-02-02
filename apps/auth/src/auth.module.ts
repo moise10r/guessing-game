6c65da81b161510a710b4e8197d8208739c40e52
moise10r <moiserushanika2006@gmail.com>
2024-02-02 17:58:38 +0400

    Add the player module under the auth service and connect to mongodb

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -2,9 +2,22 @@ import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { PlayersModule } from './players/players.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { LoggerModule } from '@app/common';
 
 @Module({
-  imports: [PlayersModule],
+  imports: [
+    PlayersModule,
+    LoggerModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- apps/auth/src/players/players.module.ts
+++ apps/auth/src/players/players.module.ts
@@ -1,7 +1,21 @@
 import { Module } from '@nestjs/common';
 import { PlayersService } from './players.service';
+import { DatabaseModule, LoggerModule } from '@app/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
+  imports: [
+    LoggerModule,
+    DatabaseModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGO_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   providers: [PlayersService],
 })
 export class PlayersModule {}
