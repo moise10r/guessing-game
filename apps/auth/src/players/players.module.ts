1212820ccd8926214e2b9fda8f128b32381f666f
moise10r <moiserushanika2006@gmail.com>
2024-02-02 16:54:30 +0400

    Create the player service

--- apps/auth/src/players/players.module.ts
+++ apps/auth/src/players/players.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PlayersService } from './players.service';
 
-@Module({})
+@Module({
+  providers: [PlayersService],
+})
 export class PlayersModule {}
