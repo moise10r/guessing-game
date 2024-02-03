3d8b11f479d3ecdd4bf19bf003a0d133bc812ab2
moise10r <moiserushanika2006@gmail.com>
2024-02-03 05:07:54 +0400

    Make the rmqmodule dynamic

--- libs/common/src/rmq/rmq.module.ts
+++ libs/common/src/rmq/rmq.module.ts
@@ -1,8 +1,33 @@
-import { Module } from '@nestjs/common';
+import { DynamicModule, Module } from '@nestjs/common';
 import { RmqService } from './rmq.service';
+import { RmqModuleOptions } from './interfaces/rmqModuleOptions.interface';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [RmqService],
   exports: [RmqService],
 })
-export class RmqModule {}
+export class RmqModule {
+  static register({ name }: RmqModuleOptions): DynamicModule {
+    return {
+      module: RmqModule,
+      imports: [
+        ClientsModule.registerAsync([
+          {
+            name,
+            useFactory: (configService: ConfigService) => ({
+              transport: Transport.RMQ,
+              options: {
+                urls: [configService.get<string>('RABBIT_MQ_URI')],
+                queue: configService.get<string>(`RABBIT_MQ_${name}_QUEUE`),
+              },
+            }),
+            inject: [ConfigService],
+          },
+        ]),
+      ],
+      exports: [ClientsModule],
+    };
+  }
+}
